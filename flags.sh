79331c15c68fe36e66bee68004373cf429618ce7
Matthew Steffen <msteffen@msteffen-macbook-4.local>
2015-06-28 18:10:28 -0700

    Initial commit, adding flags library

---
+++ flags.sh
@@ -0,0 +1,37 @@
+###################################################
+# Small bash library for simplifying the use of flags in bash scrips
+###################################################
+# Usage:
+#
+# MAKE_FLAG var_a "default value"
+# MAKE_FLAG var_b
+# PARSE_ALL_FLAGS $0 "${@}"
+# ...
+# echo "Using the value in var_a, which is ${var_a}"
+
+__FLAGS__=()
+__FILL_DEFAULTS__=()
+
+function MAKE_FLAG {
+  __FLAGS__+=("$1::")
+  __FILL_DEFAULTS__+=("$1=\"$2\"")
+}
+
+function PARSE_ALL_FLAGS {
+  # Fill default values
+  for cmd in "${__FILL_DEFAULTS__}"; do
+    eval "${cmd}"
+  done
+
+  # First element of sys.argv is "-c" so omit that by joining argv[1:] instead
+  local flags="$( python -c 'import sys; print ",".join(sys.argv[1:])' "${__FLAGS__[@]}" )"
+  local OPTS=( $(getopt -n "$1" -l "${flags}" -- ${@:1}) )
+
+  # Read all args to PARSE_ALL_FLAGS and use them to set the flag values
+  while [[ "${#OPTS[@]}" -gt 1 ]] && [[ "${OPTS[0]}" != '--' ]]; do
+    # :2 removes the "--" from the front of the long arg's name
+    eval ${OPTS[0]:2}=${OPTS[1]}
+    OPTS=( "${OPTS[@]:2}" )
+  done
+  echo "${OPTS[@]:1}"
+}
